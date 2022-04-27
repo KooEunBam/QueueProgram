d0d842d0c41299ad6349423b444223ceae3d119d
Yoon SangHoon <54784919+KooEunBam@users.noreply.github.com>
2022-04-27 15:23:53 +0900

    Add files via upload

---
+++ 9. QueueMutexLockProgram/9. QueueMutexLockProgram/Common/NewData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingQueueMutexProgram.Common
+{
+    public class NewData
+    {
+        public int seq;
+        public int data;
+
+        public NewData() // default 생성자
+        {
+
+        }
+
+        public NewData(int seq, int data)
+        {
+            this.seq = seq;
+            this.data = data;
+        }
+    }
+}
